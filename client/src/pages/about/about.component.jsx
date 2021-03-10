840c86e75d3bb06fd68737e4dcddbef13f5ea568
Jason <jason8maharjan@gmail.com>
2021-03-10 14:52:01 +0545

    About page UI added

--- client/src/pages/about/about.component.jsx
+++ client/src/pages/about/about.component.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const About = () => {
-  return <div></div>;
+  return (
+    <section className="content">
+      <p className="text-about">
+        This is a chat app named "Chat" which is very similar to Discord. <br />{" "}
+        <br />
+        You can create servers and chat rooms. Create your account and get
+        started!
+      </p>
+    </section>
+  );
 };
 
 export default About;
