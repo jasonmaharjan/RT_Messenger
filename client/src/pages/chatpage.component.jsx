4b554ee307745168895764a24ffc5ddf83bdf344
Jason <jason8maharjan@gmail.com>
2021-10-10 12:00:29 +0545

    Updated the server-list UI

---
+++ client/src/pages/chatpage.component.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectToggle } from "../redux/server/server.selectors";
+
+import ServerCol from "../components/servers-list/servers-list.component";
+import ChatRoomCol from "../components/chatrooms-list/chatrooms-list.component";
+import ChatCol from "../components/chat/chat.component";
+import MembersCol from "../components/members/members.component";
+
+import CreateServer from "../components/dialog-box/createServer.component";
+
+import { chatPage } from "../api/axios";
+
+import {
+  ChatPageContainer,
+  ServerListContainer,
+  ChatRoomsContainer,
+  ChatContainer,
+  MembersContainer,
+} from "../styles/Container";
+
+const ChatPage = ({ toggleFlag }) => {
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    chatPage(token); // check validity of token
+  }, []);
+
+  return (
+    <ChatPageContainer>
+      {toggleFlag ? <CreateServer /> : null}
+      <ServerListContainer>
+        <ServerCol />
+      </ServerListContainer>
+
+      <ChatRoomsContainer>
+        <ChatRoomCol />
+      </ChatRoomsContainer>
+
+      <ChatContainer>
+        <ChatCol />
+      </ChatContainer>
+
+      <MembersContainer>
+        <MembersCol />
+      </MembersContainer>
+    </ChatPageContainer>
+  );
+};
+
+const MapStateToProps = createStructuredSelector({
+  toggleFlag: selectToggle,
+});
+
+export default connect(MapStateToProps)(ChatPage);
---
+++ client/src/styles/ServerColumn.js
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+
+export const ServersListContainer = styled.ul`
+  margin: 0;
+  list-style-type: none;
+  padding: 1.25rem;
+`;
+
+export const Servers = styled.li`
+  height: 3rem;
+  width: 3rem;
+  margin-bottom: 1.5rem;
+  background-color: var(--grey-m-dark);
+  border-radius: 50%;
+  text-align: center;
+  position: relative;
+`;
+
+export const ServerBtn = styled.button`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  font-family: inherit;
+  color: var(--grey-light);
+  background-color: var(--grey-m-dark);
+  border-radius: 50%;
+  outline-style: none;
+  transition: all 0.2s;
+  position: relative;
+
+  &:hover {
+    background-color: var(--primary-dark);
+    cursor: pointer;
+  }
+
+  &:focus {
+    background-color: var(--primary-dark);
+  }
+`;
+
+export const ServerName = styled.span`
+  text-transform: uppercase;
+  font-size: 1rem;
+`;
+
+export const ServerSelect = styled.div`
+  position: fixed;
+  height: 3rem;
+  top: ${(props) => props.theme.serverPosition}px;
+  border-radius: 2rem;
+  width: 0.15rem;
+  background-color: var(--primary);
+  transition: all 0.2s;
+`;
